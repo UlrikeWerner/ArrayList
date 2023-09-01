c01845919da3797ca3a525d89f51e005e14b52bf
UlrikeWerner <ulrike_werner@web.de>
2023-09-01 11:05:32 +0200

    add School

--- src/main/java/com/github/UlrikeWerner/ArrayList/Main.java
+++ src/main/java/com/github/UlrikeWerner/ArrayList/Main.java
@@ -6,10 +6,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student(526183, "Anna", "Müller"));
-        studentList.add(new Student(526184, "Dieter", "Lehmann"));
-        studentList.add(new Student(526185, "Otto", "Nielsen"));
+        Student student526183 = new Student(526183, "Anna", "Müller");
+        Student student526184 = new Student(526184, "Dieter", "Lehmann");
+        Student student526185 = new Student(526185, "Otto", "Nielsen");
 
+        studentList.add(student526183);
+        studentList.add(student526184);
+        studentList.add(student526185);
         System.out.println(studentList.get(1));
+        System.out.println("-----------------------------");
+
+        School gymnasium = new School();
+        gymnasium.addStudent(student526183);
+        gymnasium.addStudent(student526185);
+        System.out.println(gymnasium);
     }
 }
